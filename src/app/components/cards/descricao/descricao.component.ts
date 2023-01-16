d553bdf2f1b76b6387d2628ad753b2a26770ee93
Leonardo <leeonardo.lf@gmail.com>
2023-01-15 21:10:23 -0300

    Lógica ComponentDescricao

--- src/app/components/cards/descricao/descricao.component.ts
+++ src/app/components/cards/descricao/descricao.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-descricao',
   templateUrl: './descricao.component.html',
-  styleUrls: ['./descricao.component.css']
+  styleUrls: ['./descricao.component.css'],
 })
 export class DescricaoComponent implements OnInit {
+  descricao: string =
+    'Gabinete Gamer Brazil PC, Mid Tower, ATX / Micro-ATX + Kit 3 Cooler GAMER ARGB, Com Lateral De Vidro';
+  limiteCaracteres = 60;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  limitarTexto(texto: string, limite: number): string {
+    if (texto.length > limite) {
+      return texto.slice(0, this.limiteCaracteres) + '...';
+    }
+    return texto;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
